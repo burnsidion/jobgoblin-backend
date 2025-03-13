ad3aa99b3f41ab5e268eff4947e77c3d87bad8dc
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-12 23:25:56 -0600

    setting up get resumes request

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import supabase from "../supabaseClient";
+import supabase from "../supabaseClient.js";
 
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import resumeRoutes from "./routes/resumeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5005;
@@ -8,8 +9,9 @@ const PORT = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/resumes", resumeRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
