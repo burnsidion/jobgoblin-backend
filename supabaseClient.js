01fd99702b804fe2b7af595db86ec64b83113201
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-13 02:23:31 -0600

    swapping anon key for service role key

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export default supabase;
